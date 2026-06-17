7a090679c09aad3720f502c3ce415cbcff6cf74e
[Daniel Sun] <dansun@stanford.edu>
2026-06-17 01:23:11 -0400

    Add Sphinx support

---
+++ docs/source/conf.py
@@ -0,0 +1,39 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "artifact-helper"
+copyright = "2026, Daniel Sun"
+author = "Daniel Sun"
+release = "0.1.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "autoapi.extension",
+    "sphinx.ext.mathjax",
+    "sphinx_tabs.tabs",
+]
+
+autoapi_dirs = ["../../artifact_helper"]
+autoapi_add_toctree_entry = False
+
+napoleon_use_param = False
+napoleon_use_ivar = True
+
+templates_path = ["_templates"]
+# exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinxdoc"
+html_static_path = ["_static"]
---
+++ docs/source/index.rst
@@ -0,0 +1,11 @@
+.. artifact-helper documentation master file, created by
+   sphinx-quickstart on Wed Jun 17 00:44:02 2026.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+artifact-helper documentation
+=============================
+
+.. toctree::
+
+   autoapi/artifact_helper/index
