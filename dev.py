6a18c3478853cf0a129bcf8906c0ba8015a5006f
[Daniel Sun] <danielcsun@hotmail.com>
2026-06-19 21:12:45 -0400

    Jupytext, prefer .py over .ipynb for repo

---
+++ dev.py
@@ -0,0 +1,40 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.19.3
+#   kernelspec:
+#     display_name: default
+#     language: python
+#     name: python3
+# ---
+
+# %%
+from pathlib import Path
+
+import numpy as np
+from matplotlib import pyplot as plt
+from tqdm.notebook import tqdm
+
+import imageio.v3 as iio
+
+from artifact_helper.preprocessing import n_frames, crop_frame, find_keyframes
+
+# %%
+media_dir = "media"
+file_format = "mkv"
+file_name = "artifacts_long"
+file_path = Path(f"./{media_dir}/{file_name}.{file_format}")
+
+# %%
+for path in Path(f"./{media_dir}").glob("*.mkv"):
+    print(f"{path.name[:-len(file_format)-1]} has {n_frames(path)} frames")
+
+# %%
+peaks = find_keyframes(file_path)
+print(f"Detected {peaks.shape[0]} unique artifacts in {file_name}.")
+
+# %%
